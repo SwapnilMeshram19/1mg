75e1fe69b2a6a7d91bc1903e02beb1616f137b84
Swapnil Arun Meshram <swapnilm1908@gmail.com>
2022-03-05 23:08:49 +0530

    cart

---
+++ js/cart.js
@@ -0,0 +1,7 @@
+import { topHeader} from "../components/header/navbar.js";
+import footer from "../components/footer/footer.js";
+
+document.querySelector(".header-sec-1").innerHTML = await topHeader();
+
+// var a=document.getElementsByTagName('ul');
+// a[0].style.display='none';
